1a95bae4ab602ee24bc4fd12bf0356bc7e6c0387
Claire Watkins <33589916+PrncssClr@users.noreply.github.com>
2026-06-21 21:09:43 +0000

    Refactor Actor class and state machine implementation; update .gitignore and VSCode settings

--- src/Sub-Scenes/Actors/util/actor.gd
+++ src/Sub-Scenes/Actors/util/actor.gd
@@ -1,26 +1,76 @@
 class_name Actor
 extends CharacterBody2D
 
+var state_machine: StateMachine = StateMachine.new()
+var action_queue: Queue = Queue.new()
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+func _ready() -> void:
+	# set self as the operator of the state machine
+	state_machine.set_operator(self)
+
+	#set up the IDLE state
+	var idle_state: StateMachine.State = StateMachine.State.new()
+	idle_state.set_enter(idle_enter)
+	idle_state.set_process(idle_process)
+	idle_state.set_exit(idle_exit)
+	state_machine.add_state("IDLE", idle_state)
+
+	var working_state: StateMachine.State = StateMachine.State.new()
+	working_state.set_enter(working_enter)
+	working_state.set_process(working_process)
+	working_state.set_exit(working_exit)
+	state_machine.add_state("WORKING", working_state)
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _process(_delta: float) -> void:
+	var dots: Array = [velocity.normalized().dot(Vector2(1,-1).normalized()),
+	 velocity.normalized().dot(Vector2(-1, -1).normalized())]
+	match dots:
+		_ when dots[0] >= 0 and dots[1] >= 0:
+			# moving north
+			pass
+		_ when dots[0] >= 0 and dots[1] <= 0:
+			# east
+			pass
+		_ when dots[0] <= 0 and dots[1] >= 0:
+			# west
+			pass
+		_ when dots[0] <= 0 and dots[1] <= 0:
+			# south
+			pass
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+#region IDLE state
+var idle_enter: Callable = func() -> void:
+	# This function is called when the IDLE state is entered.
+	# You can add any initialization code for the IDLE state here.
+	pass
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+var idle_process: Callable = func(_delta: float) -> void:
+	# This function is called every frame while the IDLE state is active.
+	# You can add any code that should run continuously in the IDLE state here.
+	pass
+var idle_exit: Callable = func() -> void:
+	# This function is called when the IDLE state is exited.
+	# You can add any cleanup code for the IDLE state here.
+	pass
+#endregion IDLE state
 
-	move_and_slide()
+
+
+#region WORKING state
+var working_enter: Callable = func() -> void:
+	# This function is called when the WORKING state is entered.
+	# You can add any initialization code for the WORKING state here.
+	pass
+
+var working_process: Callable = func(_delta: float) -> void:
+	# This function is called every frame while the WORKING state is active.
+	# You can add any code that should run continuously in the WORKING state here.
+	pass
+
+var working_exit: Callable = func() -> void:
+	# This function is called when the WORKING state is exited.
+	# You can add any cleanup code for the WORKING state here.
+	pass
+#endregion WORKING state
--- src/Utility/state_machine.gd
+++ src/Utility/state_machine.gd
@@ -2,39 +2,90 @@ class_name StateMachine
 extends Node
 
 
+## Formatted [br]{[param NAME][lb][i]String[/i][rb]:
+## [param state][lb][i]State[/i][rb], ...}[br]
+## [b]Parameters:[/b][br]
+## [param NAME] should be in constant case and match the name of the state for ease of use.
+## [br][param State] should be a State object.[br]
+## [b]NOTES:[/b][br]
+## - The state machine does not manage the lifecycle of the states,
+## so they should be created and managed externally.[br]
+## - Always initiated with a blank [param NULL] state to avoid null reference errors.[br]
+## - The NULL state acts as a safe default state and should not perform any actions.
+var states: Dictionary = {"NULL": State.new()}
 var _current_state: State = null
 
 @onready var _operator: Variant = null
 
+func _ready() -> void:
+	# Set the initial state to IDLE
+	set_state(states["NULL"])
+
 func set_operator(operator: Variant) -> void:
 	_operator = operator
+	
+## Adds a new state to the state machine.[br]
+## [b]Parameters:[/b][br]
+## [param name] should be a String representing the name of the state.[br]
+## [param state] should be a State object.
+func add_state(_name: String, _state: State) -> void:
+	states[_name] = _state
 
-func change_state(new_state: State) -> void:
-	var changed: bool = new_state != _current_state
+## Changes the current state to the new state.
+## If the new state is different from the current state,
+## the current state's exit callable is called,
+## and the new state's enter callable is called.[br]
+## [b]Parameters:[/b][br]
+## [param new_state] should be a State object.
+func change_state(_new_state: State) -> void:
+	var changed: bool = _new_state != _current_state
 	if _current_state != null:
 		_current_state.exit.call()
 	if changed:
-		_current_state = new_state
+		_current_state = _new_state
 		if _current_state != null:
 			_current_state.enter.call()
 
+## Sets the current state directly without calling the exit or enter callables.[br] 
+## [b]Parameters:[/b][br]
+## [param _state] should be a State object.
+func set_state(_state: State) -> void:
+	_current_state = _state
+
+## Processes the current state.
+## Called every frame with the delta time since the last frame.[br]
+## If there is a current state, its process callable is called with the delta time.
 func _process(delta: float) -> void:
 	if _current_state != null:
 		_current_state.process.call(delta)
 
 class State:
+	## Represents a state within the state machine.
+	## Contains callables for enter, process, and exit actions.
 
-	var enter : Callable = func()-> void: pass
+	## Callable that is called when the state is entered.
+	var _enter : Callable = func()-> void: pass
+
+	## Callable that is called every frame while the state is active.
 	var process : Callable = func(_delta: float)-> void: pass
-	var exit : Callable = func()-> void: pass
-	func new() -> State:
-		return self
 
-	func set_enter(_enter: Callable) -> void:
-		self.enter = _enter
+	## Callable that is called when the state is exited.
+	var _exit : Callable = func()-> void: pass
 
-	func set_process(_process: Callable) -> void:
-		self.process = _process
+	## Sets the enter callable for the state.
+	## [b]Parameters:[/b][br]
+	## [param _en] should be a Callable object.
+	func set_enter(_en: Callable) -> void:
+		self._enter = _en
 
-	func set_exit(_exit: Callable) -> void:
-		self.exit = _exit
+	## Sets the process callable for the state.
+	## [b]Parameters:[/b][br]
+	## [param _pr] should be a Callable object.
+	func set_process(_pr: Callable) -> void:
+		self.process = _pr
+
+	## Sets the exit callable for the state.
+	## [b]Parameters:[/b][br]
+	## [param _ex] should be a Callable object.
+	func set_exit(_ex: Callable) -> void:
+		self._exit = _ex
